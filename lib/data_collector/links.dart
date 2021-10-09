a0b8a6a6b5f408cd42ec5b3bcf45ce71286c5a8b
Nikola Grujić <grujic.nikola91@gmail.com>
2021-10-09 07:02:38 +0200

    Update links.dart

--- lib/data_collector/links.dart
+++ lib/data_collector/links.dart
@@ -1,18 +1,18 @@
 class Links {
   static const String Fixtures     = 'https://www.football-data.co.uk/fixtures.csv';
-  static const String England1     = 'https://www.football-data.co.uk/mmz4281/2021/E0.csv';
-  static const String England2     = 'https://www.football-data.co.uk/mmz4281/2021/E1.csv';
-  static const String Scotland1    = 'https://www.football-data.co.uk/mmz4281/2021/SC0.csv';
-  static const String Germany1     = 'https://www.football-data.co.uk/mmz4281/2021/D1.csv';
-  static const String Germany2     = 'https://www.football-data.co.uk/mmz4281/2021/D2.csv';
-  static const String Italy1       = 'https://www.football-data.co.uk/mmz4281/2021/I1.csv';
-  static const String Italy2       = 'https://www.football-data.co.uk/mmz4281/2021/I2.csv';
-  static const String Spain1       = 'https://www.football-data.co.uk/mmz4281/2021/SP1.csv';
-  static const String France1      = 'https://www.football-data.co.uk/mmz4281/2021/F1.csv';
-  static const String France2      = 'https://www.football-data.co.uk/mmz4281/2021/F2.csv';
-  static const String Netherlands1 = 'https://www.football-data.co.uk/mmz4281/2021/N1.csv';
-  static const String Belgium1     = 'https://www.football-data.co.uk/mmz4281/2021/B1.csv';
-  static const String Portugal1    = 'https://www.football-data.co.uk/mmz4281/2021/P1.csv';
-  static const String Turkey1      = 'https://www.football-data.co.uk/mmz4281/2021/T1.csv';
-  static const String Greece1      = 'https://www.football-data.co.uk/mmz4281/2021/G1.csv';
+  static const String England1     = 'https://www.football-data.co.uk/mmz4281/2122/E0.csv';
+  static const String England2     = 'https://www.football-data.co.uk/mmz4281/2122/E1.csv';
+  static const String Scotland1    = 'https://www.football-data.co.uk/mmz4281/2122/SC0.csv';
+  static const String Germany1     = 'https://www.football-data.co.uk/mmz4281/2122/D1.csv';
+  static const String Germany2     = 'https://www.football-data.co.uk/mmz4281/2122/D2.csv';
+  static const String Italy1       = 'https://www.football-data.co.uk/mmz4281/2122/I1.csv';
+  static const String Italy2       = 'https://www.football-data.co.uk/mmz4281/2122/I2.csv';
+  static const String Spain1       = 'https://www.football-data.co.uk/mmz4281/2122/SP1.csv';
+  static const String France1      = 'https://www.football-data.co.uk/mmz4281/2122/F1.csv';
+  static const String France2      = 'https://www.football-data.co.uk/mmz4281/2122/F2.csv';
+  static const String Netherlands1 = 'https://www.football-data.co.uk/mmz4281/2122/N1.csv';
+  static const String Belgium1     = 'https://www.football-data.co.uk/mmz4281/2122/B1.csv';
+  static const String Portugal1    = 'https://www.football-data.co.uk/mmz4281/2122/P1.csv';
+  static const String Turkey1      = 'https://www.football-data.co.uk/mmz4281/2122/T1.csv';
+  static const String Greece1      = 'https://www.football-data.co.uk/mmz4281/2122/G1.csv';
 }
