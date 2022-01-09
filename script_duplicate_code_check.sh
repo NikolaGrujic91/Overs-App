53e8ec67b2c66f026d1591f72f4f7efad95d14ae
Nikola Grujić <grujic.nikola91@gmail.com>
2022-01-09 11:03:24 +0100

    added static analysis

---
+++ script_duplicate_code_check.sh
@@ -0,0 +1,12 @@
+# https://github.com/kucherenko/jscpd
+#The jscpd gives the ability to find duplicated blocks implemented on more than
+# 150 programming languages and digital formats of documents.
+
+#install
+#npm install -g jscpd
+
+# in case of permission problems run this first
+# chmod +x ./script_duplicate_code_check.sh
+
+#usage
+npx jscpd --reporters "consoleFull" --format "dart" lib
