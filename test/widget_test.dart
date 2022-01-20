6effc232c546c457e4beba32b2a2471788e43f21
Nikola Grujić <grujic.nikola91@gmail.com>
2022-01-20 16:29:58 +0100

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:overs_app/main.dart';
+import 'package:overs/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
