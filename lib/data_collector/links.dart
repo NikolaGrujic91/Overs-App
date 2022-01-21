5f64d58f872e02dc87484b61499352534ec23e5d
Nikola Grujić <grujic.nikola91@gmail.com>
2022-01-21 06:59:00 +0100

    refactored links

--- lib/data_collector/links.dart
+++ lib/data_collector/links.dart
@@ -2,50 +2,53 @@
 // Use of this source code is governed by a GNU-style license that can be
 // found in the LICENSE file.
 
+/// API
+const api = 'https://www.football-data.co.uk';
+
 /// Fixtures url
-const kFixtures = 'https://www.football-data.co.uk/fixtures.csv';
+const kFixtures = '$api/fixtures.csv';
 
 /// England 1 url
-const kEngland1 = 'https://www.football-data.co.uk/mmz4281/2122/E0.csv';
+const kEngland1 = '$api/mmz4281/2122/E0.csv';
 
 /// England 2 url
-const kEngland2 = 'https://www.football-data.co.uk/mmz4281/2122/E1.csv';
+const kEngland2 = '$api/mmz4281/2122/E1.csv';
 
 /// Scotland 1 url
-const kScotland1 = 'https://www.football-data.co.uk/mmz4281/2122/SC0.csv';
+const kScotland1 = '$api/mmz4281/2122/SC0.csv';
 
 /// Germany 1 url
-const kGermany1 = 'https://www.football-data.co.uk/mmz4281/2122/D1.csv';
+const kGermany1 = '$api/mmz4281/2122/D1.csv';
 
 /// Germany 2 url
-const kGermany2 = 'https://www.football-data.co.uk/mmz4281/2122/D2.csv';
+const kGermany2 = '$api/mmz4281/2122/D2.csv';
 
 /// Italy 1 url
-const kItaly1 = 'https://www.football-data.co.uk/mmz4281/2122/I1.csv';
+const kItaly1 = '$api/mmz4281/2122/I1.csv';
 
 /// Italy 2 url
-const kItaly2 = 'https://www.football-data.co.uk/mmz4281/2122/I2.csv';
+const kItaly2 = '$api/mmz4281/2122/I2.csv';
 
 /// Spain 1 url
-const kSpain1 = 'https://www.football-data.co.uk/mmz4281/2122/SP1.csv';
+const kSpain1 = '$api/mmz4281/2122/SP1.csv';
 
 /// France 1 url
-const kFrance1 = 'https://www.football-data.co.uk/mmz4281/2122/F1.csv';
+const kFrance1 = '$api/mmz4281/2122/F1.csv';
 
 /// France 2 url
-const kFrance2 = 'https://www.football-data.co.uk/mmz4281/2122/F2.csv';
+const kFrance2 = '$api/mmz4281/2122/F2.csv';
 
 /// Netherlands 1 url
-const kNetherlands1 = 'https://www.football-data.co.uk/mmz4281/2122/N1.csv';
+const kNetherlands1 = '$api/mmz4281/2122/N1.csv';
 
 /// Belgium 1 url
-const kBelgium1 = 'https://www.football-data.co.uk/mmz4281/2122/B1.csv';
+const kBelgium1 = '$api/mmz4281/2122/B1.csv';
 
 /// Portugal 1 url
-const kPortugal1 = 'https://www.football-data.co.uk/mmz4281/2122/P1.csv';
+const kPortugal1 = '$api/mmz4281/2122/P1.csv';
 
 /// Turkey 1 url
-const kTurkey1 = 'https://www.football-data.co.uk/mmz4281/2122/T1.csv';
+const kTurkey1 = '$api/mmz4281/2122/T1.csv';
 
 /// Greece 1 url
-const kGreece1 = 'https://www.football-data.co.uk/mmz4281/2122/G1.csv';
+const kGreece1 = '$api/mmz4281/2122/G1.csv';
